d380fac37c900e51b9e99ea49f6ed1d126a56860
Curtis Lewis <lewis.curtis.t@gmail.com>
2018-01-18 11:35:13 -0700

    changed home page to php and universal header

---
+++ web/homepage/header.php
@@ -0,0 +1,7 @@
+<ul id="nav_bar">
+	<li id="nav_item"><a id="nav_link" href="home.php">Home</a></li>
+	<li id="nav_item"><a id="nav_link" href="assignments.html">Assignments</a></li>
+	<li id="nav_item"><a id="nav_link" href="projects.html">Projects</a></li>
+	<li id="nav_item"><a id="nav_link" href="resume.html">Resume</a></li>
+	<li id="nav_item"><a id="nav_link" href="contact.html">Contact</a></li>
+</ul>
