f4c1584e54b38553ad1e12ce263898ede46e8000
Curtis Lewis <lewis.curtis.t@gmail.com>
2018-01-18 11:50:20 -0700

    fixed a bug with no css on assginments page

--- web/homepage/assignments.php
+++ web/homepage/assignments.php
@@ -2,8 +2,8 @@
 <html>
 <head>
 	<title>Assignments</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-	<script type="text/javascript" src="script.js"></script>
+	<link rel="stylesheet" type="text/css" href="web_files/style.css">
+	<script type="text/javascript" src="web_files/script.js"></script>
 </head>
 <body>
 	<div id="header">
